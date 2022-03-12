8249e4d8b0ecfbb06b5aa3e4e3bdacd4cc45ff06
bx209 <89545607+bx209@users.noreply.github.com>
2022-03-11 19:04:48 -0800

    Add files via upload

---
+++ grocery.ts
@@ -0,0 +1,11 @@
+class Grocery {
+    name: string;
+    id: number;
+    quantity: number;
+
+    constructor(n: string, i: number, q: number) {
+        this.name = n;
+        this.id = i;
+        this.quantity = q;
+    }
+}
